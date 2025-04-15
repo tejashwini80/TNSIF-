b593cb4c5d0b0f0e785f7e6c4b898cc08837916c
tejashwini80 <kpatiltejashwini@gmail.com>
2025-04-15 18:33:40 +0530

    first commit

---
+++ src/oppsDemo/EncapMain.java
@@ -0,0 +1,17 @@
+package oppsDemo;
+
+
+
+public class EncapMain {
+
+    public static void main(String[] args) {
+        EncapDemo obj = new EncapDemo();
+        obj.setSerialNum(101);
+        obj.setName("Bunny");
+        obj.setAge(15);
+        System.out.println(obj);
+
+
+    }
+
+}
